48c9311bcd6455e6b301cdaf1cb7c4c0235a4b28
Robert Kiessling <robert.kiessling.jr@gmail.com>
2025-03-28 12:14:05 -1000

    initial vite setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import "./App.css"
+// import logo from "./logo.svg"
+
+const App = () => {
+    return (
+        <div className="app">
+            <header className="app-header">
+                Header!
+            </header>
+            <main className="main">
+                <p>
+                    Main!
+                </p>
+            </main>
+        </div>
+    )
+}
+
+export default App
